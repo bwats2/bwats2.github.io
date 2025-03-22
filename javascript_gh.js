3846ce86f52682a3e8474b8d22f215469c103fc5
Brian Watson <bwats2@gmail.com>
2025-03-22 04:42:15 +0000

    javascript issue

--- javascript_gh.js
+++ javascript_gh.js
@@ -2,10 +2,9 @@ const accessCodeInput = document.getElementById('access-code-input');
 const accessCodeSubmit = document.getElementById('access-code-submit');
 const allergiesSection = document.getElementById('allergies-section');
 const goodreadsLink = document.getElementById('goodreads-link');
-const correctCode = 'allergies'; // Store the code in a variable
 
 accessCodeSubmit.addEventListener('click', () => {
-    if (accessCodeInput.value.trim().toLowerCase() === correctCode) {
+    if (accessCodeInput.value.trim().toLowerCase() === 'allergies') {
         allergiesSection.classList.remove('hidden-section');
         allergiesSection.classList.add('show-section');
         goodreadsLink.classList.remove('hidden-section');
