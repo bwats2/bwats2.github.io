2b919c74de4bf5c0ea4e0d81e6c29da3aad16242
Brian Watson <bwats2@gmail.com>
2025-03-22 05:03:34 +0000

    hidden joke

--- javascript_gh.js
+++ javascript_gh.js
@@ -2,6 +2,7 @@ const accessCodeInput = document.getElementById('access-code-input');
 const accessCodeSubmit = document.getElementById('access-code-submit');
 const allergiesSection = document.getElementById('allergies-section');
 const goodreadsLink = document.getElementById('goodreads-link');
+const jokeInterest = document.getElementById('joke-interest');
 
 accessCodeSubmit.addEventListener('click', () => {
     if (accessCodeInput.value.trim().toLowerCase() === 'allergies') {
@@ -9,6 +10,8 @@ accessCodeSubmit.addEventListener('click', () => {
         allergiesSection.classList.add('show-section');
         goodreadsLink.classList.remove('hidden-section');
         goodreadsLink.classList.add('show-section');
+        jokeInterest.classList.remove('hidden-section');
+        jokeInterest.classList.add('show-section');
 
     } else {
         alert('Incorrect access code. Please try again.');
